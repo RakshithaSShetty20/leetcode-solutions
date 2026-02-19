5cb1c6ca674422a8186fa5deb1916ff878bada27
Rakshitha S Shetty <rakshithasshetty20@gmail.com>
2026-02-19 23:10:01 +0530

    Added Palindrome Number solution

---
+++ PalindromeNumber.java
@@ -0,0 +1,29 @@
+public class PalindromeNumber {
+
+    public static void main(String[] args) {
+
+        Solution sol = new Solution();
+
+        System.out.println(sol.isPalindrome(121));   // true
+        System.out.println(sol.isPalindrome(-121));  // false
+        System.out.println(sol.isPalindrome(10));    // false
+    }
+}
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+
+        int original = x;
+        int reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x = x / 10;
+        }
+
+        return original == reversed;
+    }
+}
+
